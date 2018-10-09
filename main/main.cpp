9c9115a107609933f75e313de6b0db9dd2f1750b
techie <techie@minzea.cn>
2018-10-10 01:24:15 +0800

    牧牛链v0.0.1版本

---
+++ main/main.cpp
@@ -0,0 +1,25 @@
+﻿/*
+	moveniu is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	moveniu is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with moveniu.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <utils/headers.h>
+
+
+void SaveWSPort();
+void RunLoop();
+int main(int argc, char *argv[]){
+	
+	return 0;
+}
+
